e4c1bae446c038cbf1a563dedb76770519f9d4a6
Vinicius Regis <vini.reg@gmail.com>
2017-05-23 12:39:09 -0300

    realocações

---
+++ index.php
@@ -0,0 +1,30 @@
+<?php 
+
+require_once("config.php");
+
+// $sql = new Sql();
+// $result = $sql->select("SELECT * FROM tb_usuarios WHERE idusuario = :ID", array(":ID"=>3));
+// echo json_encode($result);
+
+
+//Carrega um usuário no objeto
+// $usuario = new Usuario();
+// $usuario->loadById(3);
+// echo $usuario;
+
+
+//Carrega uma lista de usuários
+// $lista = Usuario::getList();
+// echo json_encode($lista);
+
+
+//Carrega uma lista de usuarios buscando pelo login
+// $search = Usuario::search('vrr20');
+// echo json_encode($search);
+
+
+//Carrega um usuário usando o login e a senha
+$usuario = new Usuario();
+$usuario->login("vrr20", "2133");
+echo $usuario;
+?>
