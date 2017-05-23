ca926d55e74c4dacad5b786e67e3ead35b4e1f97
Vinicius Regis <vini.reg@gmail.com>
2017-05-23 03:37:09 -0300

    Testes

--- class/Sql.php
+++ class/Sql.php
@@ -1,14 +1,11 @@
 <?php 
 
-class testSql extends PDO {
+class Sql extends PDO {
+
 	private $conn;
 
 	public function __construct(){
-		$this->conn = new PDO("mysql:dbname=dbphp7;host=localhost", "root", "");
-	}
-
-	private function setParam($statement, $key, $value){
-		$statement->bindParam($key, $value);
+		$this->conn = new PDO("mysql:host=localhost;dbname=dbphp7", "root", "");
 	}
 
 	private function setParams($statement, $parameters = array()){
@@ -17,14 +14,22 @@ class testSql extends PDO {
 		}
 	}
 
+	private function setParam($statement, $key, $value){
+		$statement->bindParam($key, $value);
+	}
+
 	public function query($rawQuery, $params = array()){
 		$stmt = $this->conn->prepare($rawQuery);
-
 		$this->setParams($stmt, $params);
-
 		$stmt->execute();
 		return $stmt;
 	}
+
+	public function select($rawQuery, $params = array()):array{
+		$stmt = $this->query($rawQuery, $params);
+		return $stmt->fetchAll(PDO::FETCH_ASSOC);
+	}
 }
+
 
 ?>
